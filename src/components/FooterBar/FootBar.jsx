463658f9fed59c85f90c7b3dbb0572659659c445
rryunn <rryunn@ajou.ac.kr>
2025-03-06 14:13:32 +0900

    Fix: 디자인 수정

--- src/components/FooterBar/FootBar.jsx
+++ src/components/FooterBar/FootBar.jsx
@@ -3,14 +3,20 @@ import {
   FooterContainer,
   ContentWrapper,
   AdminEmailLink,
+  MobileLineBreak,
 } from './FootBar.styles';
 
 const Footer = () => {
   return (
     <FooterContainer>
       <ContentWrapper>
-        <span>본 프로젝트는 소규모 팀 프로젝트로,</span>
-        <span> 학교와는 무관합니다.</span>
+        <span>
+          본 프로젝트는 소규모 팀 프로젝트로,{' '}
+          <MobileLineBreak>
+            <br />
+          </MobileLineBreak>
+          학교와는 무관합니다.
+        </span>
         &copy; 2025 ACM. All rights reserved.
         <br />
         관리자 이메일:{' '}
