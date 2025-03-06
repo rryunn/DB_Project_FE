1ff89c20fe48dd5433fb5fa90a86890a338b15db
rryunn <rryunn@ajou.ac.kr>
2025-03-06 13:59:27 +0900

    Fix: 디자인 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import AppRouter from "./router.jsx";
-import GlobalStyle from "./GlobalStyle";
+import React from 'react';
+import AppRouter from './router.jsx';
+import GlobalStyle from './GlobalStyle';
 
 // 사용 예
 function App() {
