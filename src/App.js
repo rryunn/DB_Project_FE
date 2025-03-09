4743d62493289bba54f5c1b7d6915558d7c59a1a
rryunn <rryunn@ajou.ac.kr>
2025-03-09 19:48:59 +0900

    fix: 재훈님 작업 취소

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import AppRouter from './router.jsx';
 import GlobalStyle from './GlobalStyle';
-import { setupAxiosInterceptors } from './axiosInterceptor';
 
 // 사용 예
 function App() {
-  useEffect(() => {
-    setupAxiosInterceptors(); // ✅ 앱이 실행될 때 한 번만 실행
-  }, []);
   return (
     <>
       <GlobalStyle />
